8d35fff5b20d60190cc7599c9bba3834cf6b5bd3
Alejandro Levesque <levesque.alejandro@gmail.com>
2023-01-06 16:44:21 -0500

    Update Results.py

--- Results.py
+++ Results.py
@@ -33,9 +33,9 @@ class Results(ResultsTemplate):
     results_raw = [[0]*len(self.designer_list) for count in range(len(self.classification_list))]
     
     #the rest just store 2D lists of values for plotting, calculated from results_raw
-    requests_per_designer_and_class = [[0]*len(self.designer_list) for count in range(len(self.classification_list))]
-    duration_per_designer_and_class = [[0]*len(self.designer_list) for count in range(len(self.classification_list))]
-    open_requests_per_designer_and_class = [[0]*len(self.designer_list) for count in range(len(self.classification_list))]
+    self.requests_per_designer_and_class = [[0]*len(self.designer_list) for count in range(len(self.classification_list))]
+    self.duration_per_designer_and_class = [[0]*len(self.designer_list) for count in range(len(self.classification_list))]
+    self.open_requests_per_designer_and_class = [[0]*len(self.designer_list) for count in range(len(self.classification_list))]
     
     #set time period to graph over
     if self.two_weeks_radio_button.selected == True:
@@ -56,7 +56,7 @@ class Results(ResultsTemplate):
         results_raw[j][i] = app_tables.requests.search(Classification=doc_classification,Designer=person,Date_Delivered=q.any_of(q.greater_than(date_period),q.all_of(None)))
         
         #to get number of requests, simply get length of that Classification/Designer pair
-        requests_per_designer_and_class[j][i] = len(results_raw[j][i])
+        self.requests_per_designer_and_class[j][i] = len(results_raw[j][i])
         
         #count results for this Classification/Designer pair that don't have completion times (ie are open)
         #otherwise, they have completion times so find the average average duration of requests
@@ -69,13 +69,14 @@ class Results(ResultsTemplate):
           else:
             closed_count += 1
             duration += y['Time_to_Completion']
+        #dont divide by zero
         if(closed_count!=0):
-          duration_per_designer_and_class[j][i] = duration//closed_count
+          self.duration_per_designer_and_class[j][i] = duration//closed_count
         else:
-          duration_per_designer_and_class[j][i] = 0
-        open_requests_per_designer_and_class[j][i] = open_count
+          self.duration_per_designer_and_class[j][i] = 0
+        self.open_requests_per_designer_and_class[j][i] = open_count
         
-    return requests_per_designer_and_class, duration_per_designer_and_class, open_requests_per_designer_and_class#results_raw
+    pass #requests_per_designer_and_class, duration_per_designer_and_class, open_requests_per_designer_and_class
   
   def logout_button_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -85,46 +86,6 @@ class Results(ResultsTemplate):
   def log_menu_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('Log')
-    pass
-
-  def req_per_period_plot_show(self, **event_args):
-    '''print('req')
-    """This method is called when the Plot is shown on the screen"""
-    plot_names = ['New Requests','Update Requests','Correction Requests']
-    for index, row in enumerate(self.requests_per_designer_and_class):
-      self.req_per_period_plot.data += [ go.Bar(y=row[:],name=plot_names[index],**self.plot_params)]
-
-    plot_layout_req = self.plot_layout
-    plot_layout_req['title'] = {
-      'text': 'Requests Per Period - {0}'.format(self.title_period)
-    }
-    self.req_per_period_plot.layout = plot_layout_req'''
-    pass
-
-  def days_to_complete_plot_show(self, **event_args):
-    '''"""This method is called when the Plot is shown on the screen"""
-    plot_names = ['New Turnaround Time','Update Turnaround Time','Correction Turnaround Time']
-    for index, row in enumerate(self.duration_per_designer_and_class):
-      self.days_to_complete_plot.data += [ go.Bar(y=row[:],name=plot_names[index],**self.plot_params)]
-    
-    plot_layout_duration = self.plot_layout
-    plot_layout_duration['title'] = {
-      'text': 'Average Days to Request Completion - {0}'.format(self.title_period)
-    }
-    self.days_to_complete_plot.layout = plot_layout_duration'''
-    pass
-
-  def open_req_plot_show(self, **event_args):
-    '''"""This method is called when the Plot is shown on the screen"""
-    plot_names = ['Open New Requests','Open Update Requests','Open Correction Requests']
-    for index, row in enumerate(self.open_requests_per_designer_and_class):
-      self.open_req_plot.data += [ go.Bar(y=row[:],name=plot_names[index],**self.plot_params)]
-    
-    plot_layout_open = self.plot_layout 
-    plot_layout_open['title'] = {
-      'text': 'Open Requests - {0}'.format(self.title_period)
-    }
-    self.open_req_plot.layout = plot_layout_open'''
     pass
 
   def all_time_radio_button_clicked(self, **event_args):
@@ -142,7 +103,8 @@ class Results(ResultsTemplate):
   def refresh_tables(self,**args):
     #TODO: may not actually redraw graphs - add data pre-two weeks and check
     #recalculate graphs
-    [self.requests_per_designer_and_class, self.duration_per_designer_and_class, self.open_requests_per_designer_and_class] = self.calculate_results()
+    #[self.requests_per_designer_and_class, self.duration_per_designer_and_class, self.open_requests_per_designer_and_class] = self.calculate_results()
+    self.calculate_results()
     self.draw_all_plots()
     '''self.days_to_complete_plot.redraw()
     self.req_per_period_plot.redraw()
@@ -172,51 +134,35 @@ class Results(ResultsTemplate):
     pass
 
   def draw_all_plots(self,**args):
-    #TODO: when new time period is clicked, old data isnt discarded, so new data is appended and there are two datasets on one plot
-    print('req')
     datasets = [self.requests_per_designer_and_class,self.duration_per_designer_and_class, self.open_requests_per_designer_and_class]
+    plot_names = ['Requests','Turnaround Time','Open Requests']
+    nuc = ['New','Update','Correction']
     graphs = [self.req_per_period_plot, self.days_to_complete_plot, self.open_req_plot]
-    plot_names = [['New Requests','Update Requests','Correction Requests'],['New Turnaround Time','Update Turnaround Time','Correction Turnaround Time'],['Open New Requests','Open Update Requests','Open Correction Requests']]
+    
+    #reset plots between calls so data doesnt stack up
+    for prev_data in graphs:
+      prev_data.data = []
     
     if(self.two_weeks_radio_button.selected):
       self.title_period = self.two_weeks_radio_button.text
     else:
       self.title_period = self.all_time_radio_button.text 
       
+    #adding plotly graphs to plot objects in graph[] plots those datasets with the given options
     for index_data,datas in enumerate(datasets):
       for index_graphs, row in enumerate(datas):
-        graphs[index_data].data += [ go.Bar(y=row[:],name=plot_names[index_data][index_graphs],**self.plot_params)]
+        graphs[index_data].data += [ go.Bar(y=row[:],name=nuc[index_graphs],**self.plot_params)]
       self.plot_layout['title'] = {
-        'text': 'Requests Per Period - {0}'.format(self.title_period)
+        'text': '{0} - {1}'.format(plot_names[index_data],self.title_period)
       }
-      graphs[index_data].layout = self.plot_layout
-    
-    ##########################
-    
-    '''
-    for index, row in enumerate(self.duration_per_designer_and_class):
-      self.days_to_complete_plot.data += [ go.Bar(y=row[:],name=plot_names[index],**self.plot_params)]
-    
-    plot_layout_duration = self.plot_layout
-    plot_layout_duration['title'] = {
-      'text': 'Average Days to Request Completion - {0}'.format(self.title_period)
-    }
-    self.days_to_complete_plot.layout = plot_layout_duration
-    
-    #################################
-    
-    
-    for index, row in enumerate(self.open_requests_per_designer_and_class):
-      self.open_req_plot.data += [ go.Bar(y=row[:],name=plot_names[index],**self.plot_params)]
-    
-    plot_layout_open = self.plot_layout 
-    plot_layout_open['title'] = {
-      'text': 'Open Requests - {0}'.format(self.title_period)
-    }
-    self.open_req_plot.layout = plot_layout_open'''
-    
-    
+      graphs[index_data].layout = self.plot_layout    
     pass
+
+  def log_menu_button_copy_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Results')
+    pass
+
 
 
 
